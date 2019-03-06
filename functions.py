f1a2009aa2b956b46be9e319e6be2d286c6efcaf
Kamil Kowalewski <szamil128@onet.pl>
2019-03-06 19:03:30 +0100

    I added most of counting values from second part of excercise

--- functions.py
+++ functions.py
@@ -1,7 +1,30 @@
 import pandas
+from scipy import stats
+import statistics
 
-def printing ():
-    summary = pandas.DataFrame(columns=['Szerokosc Dzialki Kielicha', 'Dlugosc Dzialki Kielicha', 'Szerokosc Platka', 'Dlugosc Platka'])
 
-    return summary
+def printing(incomingData):
+    inTotal = pandas.DataFrame(
+        columns=['Szer Kielicha', 'Dlug Kielicha', 'Szer Platka', 'Dlug Platka'])
 
+    inTotal.loc['Mininum'] = [i for i in incomingData.min(numeric_only=True)]
+    inTotal.loc['Maximum'] = [i for i in incomingData.max(numeric_only=True)]
+    inTotal.loc['Rozstep'] = [i for i in inTotal.loc['Maximum'] - inTotal.loc['Mininum']]
+
+    inTotal.loc['Sred Arytm'] = [i for i in incomingData.mean()]
+    inTotal.loc['Sred Geom'] = stats.gmean(incomingData.iloc[:, 0:4])
+    inTotal.loc['Sred Harm'] = stats.hmean(incomingData.iloc[:, 0:4])
+    inTotal.loc['Sred potegowa 2 rzedu '] = [i for i in (
+            ((incomingData.iloc[:, 0:4] ** 2).sum() / incomingData.shape[0]) ** (1. / 2))]
+    inTotal.loc['Sred potegowa 3 rzedu '] = [i for i in (
+            ((incomingData.iloc[:, 0:4] ** 3).sum() / incomingData.shape[0]) ** (1. / 3))]
+
+    inTotal.loc['Wariancja'] = [i for i in incomingData.var()]
+    inTotal.loc['Odchylenie standardowe'] = [i for i in incomingData.std()]
+    inTotal.loc['Kurtoza'] = stats.kurtosis(incomingData.iloc[:, 0:4], fisher=False)
+
+    # inTotal.loc['Mediana'] = statistics.median(incomingData.iloc[:, 2:4])
+
+    return inTotal
+
+# def countQuartile(num, arr):
--- main.py
+++ main.py
@@ -2,57 +2,56 @@ import matplotlib.pyplot as drawPlot
 import pandas
 import functions
 
-#LOADING DATA FROM FILE
+# LOADING DATA FROM FILE
 irisData = pandas.read_csv('Irys.data', header=None)
 
-#SEGREGATION OF SPECIFIC TYPES TO PROPER VARIABLE
+# SEGREGATION OF SPECIFIC TYPES TO PROPER VARIABLE
 irisSetosa = irisData.loc[irisData.iloc[:, 4] == 'Iris-setosa']
 irisVersicolor = irisData.loc[irisData.iloc[:, 4] == 'Iris-versicolor']
 irisVirginica = irisData.loc[irisData.iloc[:, 4] == 'Iris-virginica']
 
-#DRAWING FIRST GRAPH - SEPAL
+# DRAWING FIRST GRAPH - SEPAL
 drawPlot.figure(1)
 drawPlot.plot(irisSetosa.iloc[:, 0], irisSetosa.iloc[:, 1], 'm+', label='Iris-setosa')
 drawPlot.plot(irisVersicolor.iloc[:, 0], irisVersicolor.iloc[:, 1], 'c+', label='Iris-versicolor')
 drawPlot.plot(irisVirginica.iloc[:, 0], irisVirginica.iloc[:, 1], 'k+', label='Iris-virginica')
-drawPlot.xlabel('sepalWidth')
-drawPlot.ylabel('sepalLength')
+drawPlot.xlabel('Szerokosc Dzialki Kielicha')
+drawPlot.ylabel('Dlugosc Dzialki Kielicha')
 drawPlot.legend()
 drawPlot.show()
 
-#DRAWING SECOND GRAPH - PETAL
+# DRAWING SECOND GRAPH - PETAL
 drawPlot.figure(2)
 drawPlot.plot(irisSetosa.iloc[:, 2], irisSetosa.iloc[:, 3], 'm+', label='Iris-setosa')
 drawPlot.plot(irisVersicolor.iloc[:, 2], irisVersicolor.iloc[:, 3], 'c+', label='Iris-versicolor')
 drawPlot.plot(irisVirginica.iloc[:, 2], irisVirginica.iloc[:, 3], 'k+', label='Iris-virginica')
-drawPlot.xlabel('petalWidth')
-drawPlot.ylabel('petalLength')
+drawPlot.xlabel('Szerokosc Platka')
+drawPlot.ylabel('Dlugosc Platka')
 drawPlot.legend()
 drawPlot.show()
+
+
 
 #############################################################################################################
 
 def printSeperator():
     print('=================================================================================================')
 
+
 #############################################################################################################
 
 print('\nPodsumowanie Ogolne')
-print(functions.printing())
+print(functions.printing(irisData))
 printSeperator()
 
 print('\nPodsumowanie Iris Setosa')
-print(functions.printing())
+print(functions.printing(irisSetosa))
 printSeperator()
 
 print('\nPodsumowanie Iris Versicolor')
-print(functions.printing())
+print(functions.printing(irisVersicolor))
 printSeperator()
 
 print('\nPodsumowanie Iris Virginica')
-print(functions.printing())
+print(functions.printing(irisVirginica))
 printSeperator()
-
-
-
-
